ea8120b5e3bdf402b61997771e172a164ccd1ff6
LightDiscord <root@arnaud.sh>
2020-03-01 17:29:53 +0100

    feat: canvas

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-console.log("Hello, world!");
+const canvas = document.querySelector("canvas#view");
+const context = canvas.getContext("2d");
+
+context.fillStyle = "#000";
+context.fillRect(0, 0, 100, 100);
 
